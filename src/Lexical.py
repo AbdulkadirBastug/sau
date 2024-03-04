65e1d683bc6de171f7dfbf58892b770823522b67
AbdulkadirBastug <kadir5538@gmail.com>
2024-03-04 14:41:53 +0300

    iki

--- src/Lexical.py
+++ src/Lexical.py
@@ -1,8 +1,10 @@
 import nltk 
+import spacy
 from nltk.tokenize import wırd_tokenize
 from nltk.tag import pos_tag
 
 nltk.download('averaged_perceptron_tagger')
+nlp= spacy.load("en_cıre_web_sm")
 
 class Lexical:
     def __init__(self,text):
@@ -12,3 +14,7 @@ class Lexical:
         tagged_words= pos_tag(words)
         verbs = [word for word,tag in tagged_words if tag.startswith('VB')]
         return verbs    
+    def get_people_names(self):
+        doc = nlp(self.text)
+        names = [entity.text for entity in doc.ents if entity.label_ == "PERSON"]
+        return names
